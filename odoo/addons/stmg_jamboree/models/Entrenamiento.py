bca06757e6f1ae50388a56c5ce8fc210738e8ff8
Samuel <satomoga@yahoo.es>
2025-02-26 22:00:27 +0100

    Campos calculados en entrenamiento y sede

--- odoo/addons/stmg_jamboree/models/Entrenamiento.py
+++ odoo/addons/stmg_jamboree/models/Entrenamiento.py
@@ -4,16 +4,32 @@ class Entrenamiento(models.Model):
     _name = "stmg_jamboree.entrenamiento"
     _description = 'Entrenamiento'
     
-    name = fields.Char('id', required=True, index=True)
+    name = fields.Char('id', required=True)
     tipo = fields.Selection([
         ('personal', 'Personal'),
         ('infantil', 'Infantil'),
         ('desarrollo', 'Desarrollo'),
     ], string='Tipo')
-    turno = fields.Datetime('Turno')
+    turno = fields.Datetime('Turno', index=True)
     
     sede_id = fields.Many2one('stmg_jamboree.sede', string='Sede')
     nombre_sede = fields.Char(related='sede_id.nombre', string="Nombre de Sede")
     
     entrenador_ids = fields.Many2many('stmg_jamboree.entrenador', string='Entrenador', relation='stmg_jamboree_entrenamiento_entrenador_rel')
+    
+    
     jugador_ids = fields.Many2many('stmg_jamboree.jugador', string='Jugador', relation='stmg_jamboree_entrenamiento_jugador_rel')
+
+    lista_entrenador = fields.Text(string='Lista de Entrenadores', compute='_entrenadores_str')
+    
+    @api.depends('entrenador_ids')
+    def _entrenadores_str(self):
+        for record in self:
+            nombres_completos = ""
+            
+            for entrenador in record.entrenador_ids:
+                nombres_completos += f"\n{entrenador.nombre} {entrenador.apellidos}"
+                
+            
+            
+            record.lista_entrenador = nombres_completos
--- odoo/addons/stmg_jamboree/models/Sede.py
+++ odoo/addons/stmg_jamboree/models/Sede.py
@@ -1,4 +1,5 @@
 from odoo import models, api, fields
+from datetime import datetime
 
 class Sede(models.Model):
     _name = 'stmg_jamboree.sede'
@@ -9,3 +10,18 @@ class Sede(models.Model):
     fotografia = fields.Binary('Fotografia')
     ciudad_id = fields.Many2one('res.city', string='Ciudad')
     entrenamiento_ids = fields.One2many('stmg_jamboree.entrenamiento', 'sede_id', string='Entrenamientos')
+    
+    entrenamientos_30dias = fields.Integer(string='Entrenamientos para 30 días', compute='_contador')
+    
+    @api.depends('entrenamiento_ids')
+    def _contador(self):
+        
+        for record in self:
+            contador = 0
+            for entrenamiento in record.entrenamiento_ids:
+                resta = entrenamiento.turno - datetime.today()
+                dias = resta.days
+                if(dias < 31 and dias >= 0):
+                    contador += 1
+                    
+            record.entrenamientos_30dias = contador
