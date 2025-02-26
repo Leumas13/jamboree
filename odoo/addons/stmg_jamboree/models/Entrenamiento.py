e6883dca363d4af028163183a39f57aa94d1602d
leumas13 <stmg71@educa.madrid.com>
2025-02-26 12:26:51 +0100

    kanban y calendar entrenamientos

--- odoo/addons/stmg_jamboree/models/Entrenamiento.py
+++ odoo/addons/stmg_jamboree/models/Entrenamiento.py
@@ -11,6 +11,9 @@ class Entrenamiento(models.Model):
         ('desarrollo', 'Desarrollo'),
     ], string='Tipo')
     turno = fields.Datetime('Turno')
+    
     sede_id = fields.Many2one('stmg_jamboree.sede', string='Sede')
+    nombre_sede = fields.Char(related='sede_id.nombre', string="Nombre de Sede")
+    
     entrenador_ids = fields.Many2many('stmg_jamboree.entrenador', string='Entrenador', relation='stmg_jamboree_entrenamiento_entrenador_rel')
     jugador_ids = fields.Many2many('stmg_jamboree.jugador', string='Jugador', relation='stmg_jamboree_entrenamiento_jugador_rel')
