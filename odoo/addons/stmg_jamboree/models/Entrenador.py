3b8efd69cb02d9a40cc178d0a338a057e349e6d1
= <134961618+Leumas13@users.noreply.github.com>
2025-02-28 11:26:44 +0100

    Revert "prueba de herencia sin terminar"
    This reverts commit 00e18d182690c64757690f1719a3b21a0ab6218c.

--- odoo/addons/stmg_jamboree/models/Entrenador.py
+++ odoo/addons/stmg_jamboree/models/Entrenador.py
@@ -2,6 +2,11 @@ from odoo import models, api, fields
 
 class Entrenador(models.Model):
     _name = 'stmg_jamboree.entrenador'
-    _inherit = 'res.partner'
-
+    _description = 'Entrenador'
+    
+    name = fields.Char('id')
+    nombre = fields.Char('Nombre')
+    apellidos = fields.Char('Apellidos')
+    email = fields.Char('e-mail')
+    telefono = fields.Integer('telefono')
     entrenamiento_ids = fields.Many2many('stmg_jamboree.entrenamiento', string='Entrenamientos', relation='stmg_jamboree_entrenamiento_entrenador_rel')
